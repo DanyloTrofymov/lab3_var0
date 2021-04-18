4eac0e39dd109d7fbce3e20baeea6a23e7132f10
Olha Shevel <o.v.shevel@gmail.com>
2021-04-18 21:20:00 +0300

    main method

--- lab 3 var 0/lab 3 var 0.cpp
+++ lab 3 var 0/lab 3 var 0.cpp
@@ -1,15 +1,61 @@
 ﻿#include <iostream>
-#include "archive.h"
 #include "file_archive.h"
-#include <vector>
 #include "iarchivator.h"
 
 using namespace std;
 
-int main(/*int argc, char* argv[]*/)
+int main(int argc, char* argv[])
 {
-	FileArchive file( ArchivatorFactory::ArchivatorType::LZW);
-    file.Compress("/Users/user/Desktop/test/in.txt", "/Users/user/Desktop/test/out.bin");
+    if(argc == 4){
+    string action = argv[1];
+    string inputFile = argv[2];
+    string outputFile = argv[3];
+
+    if(
+            ((action == "--compress") || (action == "--decompress"))
+        && !inputFile.empty()
+        && !outputFile.empty()) {
+        string archivatorType ;
+            cout << "What solution algorithm would you choose? (type LZW or RLE)" << endl;
+            cin >> archivatorType;
+        if (archivatorType == "LZW") {
+            FileArchive file(ArchivatorFactory::ArchivatorType::LZW);
+            if (action == "--compress") {
+                file.Compress(inputFile, outputFile);
+                cout << "Packed successfully! The result was recorded to file "<<outputFile<<endl;
+                return 0;
+            } else if (action == "--decompress") {
+                file.Decompress(inputFile, outputFile);
+                cout << "Unpacked successfully! The result was recorded to file "<<outputFile<<endl;
+                return 0;
+            }
+        } else if (archivatorType == "RLE") {
+            FileArchive file(ArchivatorFactory::ArchivatorType::RLE);
+            {
+                file.Compress(inputFile, outputFile);
+                if (action == "--compress") {
+                    file.Compress(inputFile, outputFile);
+                    cout << "Packed successfully! The result was recorded to file "<<outputFile<<endl;
+                    return 0;
+
+                } else if (action == "--decompress") {
+                    file.Decompress(inputFile, outputFile);
+                    cout << "Unpacked successfully! The result was recorded to file "<<outputFile<<endl;
+                    return 0;
+                }
+            }
+        }
+        else {
+            cout << "Archiving method was entered incorrectly. Closing the program..."<< endl;
+            return 0;
+        }
+    }
+    } else {
+        cout << "Some parameters were entered incorrectly. Closing the program..."<< endl;
+        return 0;
+    }
+//    FileArchive file(ArchivatorFactory::ArchivatorType::LZW);
+//    file.Compress("/Users/user/Desktop/test/in.txt" , "/Users/user/Desktop/test/out.bin");
    // file.Decompress("/Users/user/Desktop/test/out.bin", "/Users/user/Desktop/test/out2.txt");
 //	file.Compress("1.jpg", "encoded1.txt");
 //	file.Decompress("encoded1.txt", "2.jpg");
