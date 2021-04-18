d93b195222cdc03986655cab481b719d0b1a363e
Olha Shevel <o.v.shevel@gmail.com>
2021-04-19 00:19:29 +0300

    main clean-up

--- lab 3 var 0/lab 3 var 0.cpp
+++ lab 3 var 0/lab 3 var 0.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include "file_archive.h"
 #include "iarchivator.h"
-#include <fstream>
+
 using namespace std;
 
 
@@ -25,16 +25,16 @@ int main(int argc, char* argv[])
             if (action == "--compress") {
                 if(file.Compress(inputFile, outputFile)){
                     cout << "Packed successfully! The result was recorded to file " << outputFile << endl;
-                    return 0;
+
                 }
-                return 0;
+
 
             } else if (action == "--decompress") {
                 if(file.Decompress(inputFile, outputFile)){
                     cout << "Unpacked successfully! The result was recorded to file " << outputFile << endl;
-                    return 0;
+
                 }
-                return 0;
+
             }
         } else if (archivatorType == "RLE") {
             FileArchive file(ArchivatorFactory::ArchivatorType::RLE);
@@ -43,16 +43,16 @@ int main(int argc, char* argv[])
                 if (action == "--compress") {
                     file.Compress(inputFile, outputFile);
                     cout << "Packed successfully! The result was recorded to file " << outputFile << endl;
-                    return 0;
+
 
                 } else if (action == "--decompress") {
                     file.Decompress(inputFile, outputFile);
                     cout << "Unpacked successfully! The result was recorded to file " << outputFile << endl;
-                    return 0;
+
                 }
             }
         }
-    }   else {
+    }  else {
             cout << "The command was entered incorrectly. Closing the program..."<< endl;
             return 0;
         }
@@ -61,12 +61,7 @@ int main(int argc, char* argv[])
         cout << "Some parameters were entered incorrectly. Closing the program..."<< endl;
         return 0;
     }
-////    FileArchive file(ArchivatorFactory::ArchivatorType::LZW);
-////    file.Compress("/Users/user/Desktop/test/in.txt" , "/Users/user/Desktop/test/out.bin");
-//   // file.Decompress("/Users/user/Desktop/test/out.bin", "/Users/user/Desktop/test/out2.txt");
-////	file.Compress("1.jpg", "encoded1.txt");
-////	file.Decompress("encoded1.txt", "2.jpg");
-//cout << file_exists(inputFile) << endl;
 
 
+return 0;
 }
